0356ade408cc42fe06d26f435bdd7db119c3dffa
kitamura-lab <ykitamura@kwansei.ac.jp>
2025-11-24 08:55:53 +0900

    キャッシュ削除

--- README.md
+++
@@ -1,9 +0,0 @@
-# インストール
-* mmpose/README.mdに従いインストールする．
-* MotionBERT/README.mdに従いインストールする．
-
-# 利用法
-```
-.\run.ps1 動画へのパス
-```
-推定結果はoutputフォルダに保存されます．
--- run.ps1
+++
@@ -1,9 +0,0 @@
-Write-Output "mmpose実行"
-conda activate mmpose
-python .\mmpose\run_hrnet.py $Args[0]
-python .\mmpose\dark-coco.py
-Write-Output "MotionBERT実行"
-conda activate motionbert
-Set-Location .\MotionBERT
-python .\choose_file_from_file_manager.py --input ..\$Args --o ..\output
-Set-Location ..
